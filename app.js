08978064c401720dd36ff362fbf0a56cdded2af9
C/kadir <ccmboyccmboy7@gmail.com>
2026-04-16 16:50:37 +0300

    second screen ayay ku daray

--- app.js
+++ app.js
@@ -6,21 +6,40 @@ const screen_one=document.querySelector("#screen_one");
 const theme_toggle=document.querySelector("#theme_toggle");
 const screens=document.querySelector(".screens")
 const salan_magaca=document.querySelector("#salan_magaca ")
+ const next_arrow=document.querySelector("#next_arrow")
 
 
-sign_up_form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    screen_one.classList.replace("md:flex","hidden")
-    screen_two.classList.remove("hidden")
-    const magac=document.querySelector("#name_loging").value;
-    salan_magaca.textContent=magac;
+sign_up_form.addEventListener("submit", (e) => {
+    e.preventDefault();
     
-})
+    // Qabo magaca qofka qoray
+    const magaca_la_qoray = document.querySelector("#name_loging").value;
+    
+    // Animation-ka Arrow-ga
+    next_arrow.classList.add("translate-x-full", "transition-transform");
+
+    setTimeout(() => {
+        screen_one.classList.replace("md:flex", "hidden");
+        screen_two.classList.remove("hidden");
+        
+        // Halkaan ku dar magaca qofka
+        salan_magaca.textContent = magaca_la_qoray;
+    }, 500);
+});
 
 theme_toggle.addEventListener("click",(e)=>{
 
 document.documentElement.classList.toggle("dark");
 theme_toggle.classList.toggle("text-orange-200")
+if (document.documentElement.classList.contains("dark")) {
+
+ 
+        theme_icon.classList.replace("fa-moon", "fa-sun");
+    } else {
+        theme_icon.classList.replace("fa-sun", "fa-moon");
+    }
+
+
 })
 
 
