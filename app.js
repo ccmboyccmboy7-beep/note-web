2f4d8e9abbd6504d07b21565c4c324eef172582d
C/kadir <ccmboyccmboy7@gmail.com>
2026-04-18 21:13:56 +0300

    personal  save screen ayay ku daray

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const screen_one=document.querySelector("#screen_one");
 const theme_toggle=document.querySelectorAll(".theme_toggle");
 const screens=document.querySelector(".screens")
 const salan_magaca=document.querySelector("#salan_magaca ")
- const next_arrow=document.querySelector("#next_arrow")
+const next_arrow=document.querySelector("#next_arrow")
 const gb_space=document.querySelector("#gb_space");
 const screen_three=document.querySelector("#screen_three")
 const personal_btn=document.querySelector("#personal_btn")
+const text_area_personal=document.querySelector("#text_area_personal");
+const words_counter=document.querySelector(".words_counter");
+const save_btn=document.querySelector(".save_btn");
 
 
 sign_up_form.addEventListener("submit", (e) => {
@@ -49,7 +52,7 @@ theme_toggle.forEach((btn) => {
     });
 });
 
-// peroal_btn addvent switch screen
+// personal_btn addvent switch screen
 personal_btn.addEventListener("click",e=>{
     setTimeout(() => {
         screen_two.classList.add("hidden")
@@ -58,3 +61,20 @@ screen_three.classList.remove("hidden")
 
 })
 
+// personal noteka meesha lagu qoro save
+save_btn.addEventListener("click",e=>{
+    e.preventDefault();
+screen_three.classList.add("hidden")
+screen_two.classList.remove("hidden")
+const text_area_personal_saved_text=text_area_personal.value ;
+
+personal_btn.addEventListener("click",e=>{
+    setTimeout(() => {
+        screen_two.classList.add("hidden")
+screen_three.classList.remove("hidden")
+text_area_personal+=text_area_personal_saved_text;
+
+    }, 500);
+
+})
+});
